0acf11a4586657ed9d1829944313e33c85ccd434
berger <robertberger.dev@gmail.com>
2023-02-07 16:44:59 -0500

    added .map to desplay all of the contacts

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import contacts from './Contacts.js';
 
 function App() {
-  let contact = contacts[0]
-  return (
-    <div className="App">
+  let contactList = contacts.map( (contact) => {
+    return (
+    <li>
       <div>Name: {contact.first_name.toUpperCase()} {contact.last_name.toUpperCase()}</div>
       <div>Email: <a href={`mailto:${contact.email}`}>{contact.email}</a></div>
       <div>Phone: {contact.phone}</div>
+    </li>
+  )})
 
+  return (
+    <div className="App">
+      <ul>{contactList}</ul>
       <div>Total Number of Contacts: {contacts.length}</div>
     </div>
   );
