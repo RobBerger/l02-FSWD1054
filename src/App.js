dcabf889664040b5fc23e327b97fa26706c83ad9
berger <robertberger.dev@gmail.com>
2023-02-07 16:22:48 -0500

    added a name variable

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    
+  let name = 'Robert';
+  
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Hello {name}!</h1>
     </div>
   );
 }
