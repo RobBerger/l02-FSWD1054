0cc533d3d068a4fefb462c69be3448d2f484a429
berger <robertberger.dev@gmail.com>
2023-02-07 16:31:08 -0500

    used map to loop through dogs array and creat a list item

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import './App.css';
 function App() {
     
   let name = 'Robert';
+  let dogs = ['Fido', 'Buffy', 'Rex'];
   
   return (
     <div className="App">
       <h1>Hello {name}!</h1>
+      <ul>
+        {
+          dogs.map(dog => (<li>{dog}</li>))
+        }
+      </ul>
     </div>
   );
 }
