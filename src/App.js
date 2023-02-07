d871f63dd27adbf01ac0fde00084b51737218d7b
berger <robertberger.dev@gmail.com>
2023-02-07 16:43:00 -0500

    exported contacts as the default for Contacts.js and changed the app code to example in App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import contacts from './Contacts.js';
 
 function App() {
-    
-  let name = 'Robert';
-  let dogs = ['Fido', 'Buffy', 'Rex'];
-  
+  let contact = contacts[0]
   return (
     <div className="App">
-      <h1>Hello {name}!</h1>
-      <ul>
-        {
-          dogs.map(dog => (<li>{dog}</li>))
-        }
-      </ul>
+      <div>Name: {contact.first_name.toUpperCase()} {contact.last_name.toUpperCase()}</div>
+      <div>Email: <a href={`mailto:${contact.email}`}>{contact.email}</a></div>
+      <div>Phone: {contact.phone}</div>
+
+      <div>Total Number of Contacts: {contacts.length}</div>
     </div>
   );
 }
